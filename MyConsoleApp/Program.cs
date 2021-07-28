ed43e199c67d2d99ae67b3ebe8ac7dbd7a94bf40
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-07-28 13:59:48 +0200

    add code 2.7 read data from database

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -13,16 +13,25 @@ namespace MyConsoleApp
         {
             //Connection to SQLite
             SQLiteConnection sqlConnection = new SQLiteConnection();
-            sqlConnection.ConnectionString = "data source=C:\\Users\\user\\Desktop\\SQLite Course\\Test.db";
+            sqlConnection.ConnectionString = "data source=C:\\Users\\user\\Desktop\\SQLite Course\\MyConsoleApp\\Test.db";
             sqlConnection.Open();
 
             //Create a SQL Command
+            SQLiteCommand sqlCommand = new SQLiteCommand();
 
             //Fill SQL command parameters
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandType = System.Data.CommandType.Text;
+            sqlCommand.CommandText = "select * from Customers";
 
             //Execute the command
+            SQLiteDataReader dataReader = sqlCommand.ExecuteReader();
 
             //Read data and show to the console
+            while (dataReader.Read())
+            {
+                Console.WriteLine("ID: {0}, Name: {1}, Phone: {2}", dataReader.GetInt16(0), dataReader.GetString(1), dataReader.GetString(2));
+            }
 
             Console.Write(sqlConnection.State);
             Console.ReadKey();
