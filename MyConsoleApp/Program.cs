0ac68513ef2b2ac87f6d2e51720070b90d34b576
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-07-28 14:11:36 +0200

    add code 2.8 insert Data into Database

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,15 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SQLite;
-
-namespace MyConsoleApp
+﻿namespace MyConsoleApp
 {
-    class Program
+    using System;
+    using System.Data.SQLite;
+
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
+        {
+            //Connection to SQLite
+            SQLiteConnection sqlConnection = new SQLiteConnection();
+            sqlConnection.ConnectionString = "data source=C:\\Users\\user\\Desktop\\SQLite Course\\MyConsoleApp\\Test.db";
+            sqlConnection.Open();
+
+            //Create a SQL Command
+            SQLiteCommand sqlCommand = new SQLiteCommand();
+
+            //Fill SQL command parameters
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandType = System.Data.CommandType.Text;
+            sqlCommand.CommandText = "Insert Into Customers (Name, Phone) Values('Ford', '03117722443')";
+
+
+            //Execute the command
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            //Check the results
+            ReadData();
+
+            Console.ReadKey();
+        }
+
+        internal static void ReadData()
         {
             //Connection to SQLite
             SQLiteConnection sqlConnection = new SQLiteConnection();
