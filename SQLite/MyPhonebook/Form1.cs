d7056ff9232cebe4804e90ee015d50a630eb9cc1
NancySchmeisser <nancy.schmeisser@netzalist.de>
2021-07-29 11:35:12 +0200

    add code 3.15 Implement read operation

--- SQLite/MyPhonebook/Form1.cs
+++ SQLite/MyPhonebook/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,31 @@ namespace MyPhonebook
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Connection to SQLite
+            SQLiteConnection sqlConnection = new SQLiteConnection();
+            sqlConnection.ConnectionString = "data source=C:\\Users\\Alireza\\Desktop\\SqliteCourse\\Phonebook.db";
 
+            //Define Select statement
+            string commandText = "Select * from Persons";
+
+            //Create a datatable to save data in memory
+            var datatable = new DataTable();
+
+            //Create a data adapter 
+            //The DataAdapter can perform Select , Insert , Update and Delete SQL operations in the Data Source.
+            //The Insert, Update and Delete SQL operations, we are using the continuation of the Select command 
+            //perform by the DataAdapter. That is the DataAdapter uses the Select statements to fill a DataSet 
+            //and use the other three SQL commands(Insert, Update, delete) to transmit changes back to the Database.
+
+            SQLiteDataAdapter myDataAdapter = new SQLiteDataAdapter(commandText, sqlConnection);
+
+            sqlConnection.Open();
+
+            //Fill data from database into a datatable
+            myDataAdapter.Fill(datatable);
+            sqlConnection.Close();
+
+            dataGridView.DataSource = datatable;
         }
 
     }
